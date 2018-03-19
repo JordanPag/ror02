b0eb9aa057cfa0a9dca6a66600701ea18a776b29
JordanPag <JordanPag@users.noreply.github.com>
2018-03-19 11:31:26 -0400

    Got google sign in working

--- Final Project/FightingGame/db/schema.rb
+++ Final Project/FightingGame/db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310014131) do
+ActiveRecord::Schema.define(version: 20180319150114) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
